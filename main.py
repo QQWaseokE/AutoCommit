a7b0a2e3ae734a8497da70f7abbd3b92b1ebb950
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-02-19 07:58:30 +0900

    불필요한 시간 단축

--- main.py
+++ main.py
@@ -15,46 +15,28 @@ browser = p.chromium.launch(headless=False)
 page = browser.new_page()
 
 page.goto("https://github.com/login")
-
 time.sleep(3)
 
 page.fill("input#login_field", id)
 page.fill("input#password", password)
-
-time.sleep(1)
-
 page.click("input[type=submit]")
-
 time.sleep(3)
 
 page.click("span[class=Button-content]")
-
 smsCode = input("Authentication code : ")
 
-page.fill("input#sms_totp", smsCode)
-
-time.sleep(3)
+page.fill("input#sms_totp", smsCode)  # 웹페이지에 직접 입력했을 경우도 고려해보자
 
 page.goto(f"https://github.com/{id}/{repository}/new/main")
-
-time.sleep(5)
+time.sleep(3)
 
 page.get_by_placeholder("Name your file...").fill(commit)
-
 page.fill("div[class=cm-line]", ".")
-
-time.sleep(1)
-
 page.click('button[data-hotkey="Mod+s"]')
-
 time.sleep(3)
 
 page.get_by_placeholder("Add an optional extended description..").fill(".")
-
-time.sleep(3)
-
 page.click('button[data-hotkey="Mod+Enter"]')
-
 time.sleep(10)
 
 p.stop()
