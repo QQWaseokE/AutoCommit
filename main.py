3e5b1baae50792889e1176a0df8a61ffdf9fe2a5
QQWaseokE <127533265+QQWaseokE@users.noreply.github.com>
2024-02-23 00:29:34 +0900

    제목에 날짜를 자동으로 입력

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+from datetime import datetime
 import time
 
 # from bs4 import BeautifulSoup
@@ -8,7 +9,7 @@ p = sync_playwright().start()
 id = input("아이디(ID) : ")
 password = input("비밀번호(Password) : ")
 repository = input("커밋 할 Repository를 입력하세요. : ")
-commit = input("커밋 제목을 입력하세요. : ")
+# commit = input("커밋 제목을 입력하세요. : ")
 
 browser = p.chromium.launch(headless=False)
 
@@ -26,11 +27,11 @@ page.click("span[class=Button-content]")
 smsCode = input("Authentication code : ")
 
 page.fill("input#sms_totp", smsCode)  # 웹페이지에 직접 입력했을 경우도 고려해보자
-
 page.goto(f"https://github.com/{id}/{repository}/new/main")
 time.sleep(3)
 
-page.get_by_placeholder("Name your file...").fill(commit)
+title_today = datetime.now().strftime("%Y%m%d")
+page.get_by_placeholder("Name your file...").fill(title_today)
 page.fill("div[class=cm-line]", ".")
 page.click('button[data-hotkey="Mod+s"]')
 time.sleep(3)
